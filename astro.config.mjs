aced966fbada9a89c61bc7bf5b8d7be00a6d7383
Suwit <suwitlichoeiwara@gmail.com>
2025-06-17 15:27:47 +0700

    seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://PhonnapaStudio.github.io",
+  site: "https://phonnapastudio.github.io/pnp-landing/",
   base: "/pnp-landing/",
   vite: {
     plugins: [tailwindcss()],
   },
+  integrations: [sitemap()],
 });
